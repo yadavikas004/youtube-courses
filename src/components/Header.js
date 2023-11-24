d07106773a88408d546fb3e509e0ea5c335b4366
Vikas Yadav <yadavikas088@gmail.com>
2023-11-25 00:50:37 +0530

    changed some files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <h1>Header</h1>
+            <h1>Learning Code</h1>
+            <p>This is header componet</p>
+        </div>
+
+    );
+}
+export default Header;
