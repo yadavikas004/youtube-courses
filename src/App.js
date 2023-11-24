9924f2c7c49c6c3c7ec67e511e4c6730717aa857
Vikas Yadav <yadavikas088@gmail.com>
2023-11-25 02:05:06 +0530

    third party depdencies added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import { Button } from 'reactstrap';
+// import Header from './components/Header';
 
 function App() {
   return (
-    <div style={{background:'cyan',padding:20}}>
-      <Header name="Vikas Yadav" title="First Card"/>
-      <hr/>
-      <Header name="Kareena Yadav" title="Second Card"/>
-      <hr/>
-      <Header name="Harsh Yadav" title="Third Card"/>
-      <hr/>
-      <h1>This is my ReactJS Application</h1>
-      <h1>Learning Something New</h1>
+    <div>
+      <h1>Simple Application</h1>
+      <Button outline color="primary">First React Button</Button>
     </div>
-    
   );
 }
 
