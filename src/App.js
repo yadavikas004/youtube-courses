b079917b8174c4a856971e92406fd0100394bae8
Vikas Yadav <yadavikas088@gmail.com>
2023-11-26 03:34:30 +0530

    jumbotron added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
-// import Home from './components/Home';
+import { ToastContainer } from 'react-toastify'; // here toast removed
+import Home from './components/Home';
 // import Header from './components/Header';
 
 function App() {
 
-  const btnHandle = () => {
-    toast.success("done...",{
-      position:"top-center"
-    });
-  }
+  // const btnHandle = () => {
+  //   toast.success("done...",{
+  //     position:"top-center"
+  //   });
+  // }
 
   return (
     <div>
       <ToastContainer />
-      <h1>Application Started</h1>
-      <Button outline color="primary" onClick={btnHandle}>First React Button</Button>
+      <Home/>
+      {/* <h1>Application Started</h1> */}
+      {/* <Button outline color="primary" onClick={btnHandle}>First React Button</Button> */}
 
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
 import React from "react";
-import {Container,Button} from "reactstrap";
+import { Container, Button } from "reactstrap";
+import { toast } from 'react-toastify';
 import '../App.css';
 
 
 const Home = () => {
-    return(
+    const btnHandle = () => {
+        toast.success("done...",{
+          position:"top-center"
+        });
+      }
+
+    return (
         <div>
-            <div class="jumbotron">
-                <h1>Start Coding With Fun</h1>
-                <p>This is developed by Me for learning purpose backend is on spring boot and frontend on reactjs</p>
+            <div className="jumbotron">
+                <h1 className="display-4"> Start Coding With Fun</h1>
+                <p className="lead">This is developed by Me for learning purpose backend is on spring boot and frontend on reactjs</p>
                 <Container>
-                    <Button color="primary">Start Using</Button>
+                    <Button color="primary" onClick={btnHandle}>Start Using</Button>
                 </Container>
+                <hr className="my-4" />
+                <p>
+                    You can add more content or features here.
+                </p>
             </div>
 
         </div>
