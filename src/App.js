3b4d02fa0cda03dda2c55f6b0e8e09212c5d45c0
Vikas Yadav <yadavikas088@gmail.com>
2023-11-26 01:52:23 +0530

    button issue resolved and added react-toastify

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@
 import React from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 // import Header from './components/Header';
 
 function App() {
+
+  const btnHandle = () => {
+    toast("this is my first message");
+  }
+
   return (
     <div>
-      <h1>Simple Application</h1>
-      <Button outline color="primary">First React Button</Button>
+      <ToastContainer />
+      <h1>Application Started</h1>
+      <Button outline color="primary" onClick={btnHandle}>First React Button</Button>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
