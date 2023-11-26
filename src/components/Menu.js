538669481441a31e46fab18ba0deeb54ec668b99
Vikas Yadav <yadavikas088@gmail.com>
2023-11-27 00:57:19 +0530

    rolled back to listgroupitem

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { ListGroup, Link } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 const Menu = () => {
 
     return (
         <ListGroup>
-            <Link className="list-group-item list-group-item-action" tag="a" to="/" action>Home</Link>
-            <Link className="list-group-item list-group-item-action" tag="a" to="/add-course" action>Add Course</Link>
-            <Link className="list-group-item list-group-item-action" tag="a" to="/view-course" action>View Courses</Link>
-            <Link className="list-group-item list-group-item-action" tag="a" to="#!" action>About</Link>
-            <Link className="list-group-item list-group-item-action" tag="a" to="#!" action>Home</Link>
+            <ListGroupItem tag="a" href="/" action>Home</ListGroupItem>
+            <ListGroupItem tag="a" href="/add-course" action>Add Course</ListGroupItem>
+            <ListGroupItem tag="a" href="/view-course" action>View Courses</ListGroupItem>
+            <ListGroupItem tag="a" href="#!" action>About</ListGroupItem>
+            <ListGroupItem tag="a" href="#!" action>Home</ListGroupItem>
         </ListGroup>
     );
 }
