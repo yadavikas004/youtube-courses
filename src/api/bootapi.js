5dda035605a5186d2c683ecb6f3bf76b20774073
Vikas Yadav <yadavikas088@gmail.com>
2023-11-27 09:14:05 +0530

    from backend data fetched at console

---
+++ src/api/bootapi.js
@@ -0,0 +1,2 @@
+const BASE_URL = 'http://localhost:8082';
+export default BASE_URL;
--- src/components/Allcourses.js
+++ src/components/Allcourses.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Course from "./Course";
 // import { Button } from "reactstrap";
+import BASE_URL from "../api/bootapi";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Allcourses = () => {
 
@@ -8,11 +11,32 @@ const Allcourses = () => {
         // alert("testing");
         document.title = "All Courses";
     },[]);
-    const [courses] = useState([
-        {title:"Java Course", description: "This is Java Learning Course"},
-        {title:"Python Course", description: "This is Python Learning Course"},
-        {title:"Nodejs Course", description: "This is Nodejs Learning Course"}
-    ])
+
+    //function to call url
+    const getAllCoursesFromServer = () => {
+        axios.get(`${BASE_URL}/courses`).then(
+            (response) =>{
+                // console.log(response);
+                console.log(response.data);
+                toast.success("course has been reloaded");
+                setCourses(response.data);
+            },
+            (error)=>{
+                console.log(error);
+                toast.error("something went wrong");
+            }
+        )
+    }
+
+    useEffect(() => {
+        getAllCoursesFromServer();
+    },[]);
+
+    // const [courses] = useState([
+    //     {title:"Java Course", description: "This is Java Learning Course"},
+    //     {title:"Python Course", description: "This is Python Learning Course"},
+    //     {title:"Nodejs Course", description: "This is Nodejs Learning Course"}
+    // ])
 
     return (
         <div>
